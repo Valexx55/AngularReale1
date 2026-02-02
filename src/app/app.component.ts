bb090bd1ab0ad403f9472c002d585dfb756daa14
Val <valexx55@gmail.com>
2026-02-02 13:26:30 +0100

    enrutamiento dni e imc

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,19 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'ngreale'; //atributos
+  title = 'ANGULAR REALE'; //atributos
   //en esta clase, el js, su funcionalidad, va a aquí
   //métodos
   constructor()
   {
-    console.log('En el constructor de AppComponent')
+    console.log('En el constructor')
+    this.title = 'ng reale 2'
   }
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { DniComponent } from './components/dni/dni.component';
+import { ImcComponent } from './components/imc/imc.component';
 
-export const routes: Routes = [];
+//las rutas se mapean/asocian en este array
+
+export const routes: Routes = [
+    {path:'dni', component: DniComponent},
+    {path:'imc', component: ImcComponent}
+];
