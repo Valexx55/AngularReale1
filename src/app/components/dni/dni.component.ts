631921165a906ead903390eabf3069786618d155
Val <valexx55@gmail.com>
2026-02-02 14:01:34 +0100

    ciclo vida dni

--- src/app/components/dni/dni.component.ts
+++ src/app/components/dni/dni.component.ts
@@ -1,11 +1,37 @@
-import { Component } from '@angular/core';
-
+import { AfterViewInit, Component, OnDestroy, OnInit } from '@angular/core';
+/**
+ *TODO: NUESTRA APP, TIENE QUE OBTENER EL DNI DEL USUARIO
+ Y DECIRLE CUÁL ES SU LETRA
+ *
+ * @export
+ * @class DniComponent
+ */
 @Component({
   selector: 'app-dni',
   imports: [],
   templateUrl: './dni.component.html',
   styleUrl: './dni.component.css'
 })
-export class DniComponent {
+export class DniComponent implements OnInit, OnDestroy, AfterViewInit{
+
+  numero!:number //non null assertion operator
+  letra!:string //otra opción, inicializiar en el constructor y otra en el tsconfig json "strictNullChecks": false
+
+  constructor() {
+    /*this.numero = 0
+    this.letra= ''*/
+    console.log("En el constructor de DNI ")
+  }
+  ngOnInit(): void {
+   console.log("En ngOnInit de DNI ")
+  }
+  ngOnDestroy(): void {
+    console.log("En ngOnDestroy de DNI ")
+  }
+
+  //onload a nivel de C
+  ngAfterViewInit(): void {
+    console.log("En ngAfterViewInit de DNI ")
+  }
 
 }
